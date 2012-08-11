3e23ffd7d22019733a048c0c5841998c3eb9aef2
Tim Baumann <tim@timbaumann.info>
2012-08-11 17:53:35 +0200

    Fix warnings: add type signatures in Data.Aeson.Schema.Choice

--- src/Data/Aeson/Schema/Choice/TH.hs
+++ src/Data/Aeson/Schema/Choice/TH.hs
@@ -18,7 +18,7 @@ generateChoice n = do
   conNames <- mapM newName $ map (\i -> "Choice" ++ show i ++ "of" ++ show n) [1..n]
   let cons = zipWith normalC conNames $ map ((:[]) . strictType notStrict) tyParams
   dataDec <- dataD (cxt []) tyName (map PlainTV tyParamNames) cons [''Eq, ''Ord, ''Show, ''Read]
-  let tyCon = foldl appT (conT tyName) tyParams
+  let tyCon = appConT tyName tyParams
   let genClassConstraints c = cxt $ map (classP c . singleton) tyParams
   instToJSON <- instanceD (genClassConstraints ''ToJSON)
                           (appT (conT ''ToJSON) tyCon)
@@ -32,20 +32,44 @@ generateChoice n = do
                                                       []
                                               ]
                             ]
-  choiceFun <- funD (mkName $ "choice" ++ show n)
+  let choiceN = mkName $ "choice" ++ show n
+  let typeAs = mkNames 'a'
+  let typeBs = mkNames 'b'
+  choiceFunDec <- sigD choiceN
+                     $ forallT (map PlainTV $ typeAs ++ typeBs)
+                               (cxt [])
+                             $ functionT (zipWith arrT (map varT typeAs) (map varT typeBs))
+                                       $ appConT tyName (map varT typeAs) `arrT` appConT tyName (map varT typeBs)
+  choiceFun <- funD choiceN
                   $ let f = mkName "f"
                         v = mkName "v"
                     in zipWith (\i con -> clause (replicate i wildP ++ [varP f] ++ replicate (n-i-1) wildP ++ [conP con [varP v]])
                                                  (normalB $ conE con `appE` (varE f `appE` varE v))
                                                  []) [0..] conNames
-  choiceIofNFuns <- forM (zip [1..n] conNames) $ \(i, con) -> do
-    funD (mkName $ "choice" ++ show i ++ "of" ++ show n ++ "s")
-       $ let cs = mkName "cs"
-             c  = mkName "c"
-         in [clause [varP cs] (normalB $ compE [bindS (conP con [varP c]) (varE cs), noBindS (varE c)]) []]
-  return $ [dataDec, instToJSON, instFromJSON, choiceFun] ++ choiceIofNFuns
+  choiceIofNFuns <- fmap concat $ forM (zip [1..n] conNames) $ \(i, con) -> do
+    let choiceIofN = mkName $ "choice" ++ show i ++ "of" ++ show n ++ "s"
+    typeDec <- sigD choiceIofN
+                  $ forallT (map PlainTV $ tyParamNames)
+                            (cxt [])
+                          $ appT listT (appConT tyName (map varT tyParamNames)) `arrT` appT listT (tyParams !! (i-1))
+    let cs = mkName "cs"
+        c  = mkName "c"
+    funDef <- funD choiceIofN
+                   [clause [varP cs] (normalB $ compE [ bindS (conP con [varP c]) (varE cs)
+                                                      , noBindS (varE c)
+                                                      ]) []]
+    return [typeDec, funDef]
+  return $ [dataDec, instToJSON, instFromJSON, choiceFunDec, choiceFun] ++ choiceIofNFuns
   where
     singleton :: a -> [a]
     singleton = (:[])
     genToJSONClause :: Name -> Name -> ClauseQ
     genToJSONClause con param = clause [conP con [varP param]] (normalB . appE (varE 'toJSON) . varE $ param) []
+    mkNames :: Char -> [Name]
+    mkNames ch = map (mkName . (ch:) . show) [1..n]
+    arrT :: TypeQ -> TypeQ -> TypeQ
+    arrT a b = arrowT `appT` a `appT` b
+    functionT :: [TypeQ] -> TypeQ -> TypeQ
+    functionT ins out = foldr arrT out ins
+    appConT :: Name -> [TypeQ] -> TypeQ
+    appConT con params = foldl appT (conT con) params
