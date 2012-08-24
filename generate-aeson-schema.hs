4bbb1c855641fdb820be95c422f15ce39694b0fc
Tim Baumann <tim@timbaumann.info>
2012-08-24 17:58:44 +0200

    build executable schema generator with cabal

--- generate-aeson-schema.hs
+++ generate-aeson-schema.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Main where
 
 import Prelude hiding (readFile)
 import Control.Monad (forM_)
-import Data.Either (either)
-import System.Environment (getArgs)
+import Data.Version (showVersion)
+import System.Console.CmdArgs
 import Data.Aeson.Parser (json)
 import Data.ByteString (readFile)
 import Data.Attoparsec.ByteString (parse, Result, IResult (..))
@@ -15,19 +17,28 @@ import qualified Data.Aeson as A
 import Data.Text (Text, pack, unpack)
 import qualified Data.Map as M
 
+import Paths_aeson_schema (version)
 import Data.Aeson.Schema (Schema (..), V3)
+
+data GenerateArgs = GenerateArgs
+  { inputSchema :: FilePath
+  } deriving (Data, Show, Typeable)
+
+generateArgs :: GenerateArgs
+generateArgs = GenerateArgs
+  { inputSchema = "" &= typFile &= argPos 0
+  } &= summary ("generate-aeson-schema-" ++ showVersion version)
 
 main :: IO ()
 main = do
-  [filename] <- getArgs
-  contents <- readFile filename
+  args <- cmdArgs generateArgs
+  contents <- readFile $ inputSchema args
   value <- iResultM $ parse json contents
   case fromJSON value :: A.Result (Schema V3 Text) of
     A.Error str -> fail str
     A.Success schema -> do
       let m = M.fromList [(pack "A", fmap undefined schema)]
       code <- runQ $ generate m
-      print code
       forM_ code $ putStrLn . either unpack pprint
   where
     iResultM :: (Monad m) => Result a -> m a
