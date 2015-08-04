6df01ebb574b8ce741dc29dc34a66c3b4f9cb7b8
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2015-08-04 10:04:05 +0100

    Work around TH Ppr instance bug for LetS case
    https://ghc.haskell.org/trac/ghc/ticket/10734

--- src/Data/Aeson/Schema/CodeGen.hs
+++ src/Data/Aeson/Schema/CodeGen.hs
@@ -199,7 +199,7 @@ assertStmt :: ExpQ -> String -> StmtQ
 assertStmt expr err = noBindS [| unless $(expr) (fail err) |]
 
 assertValidates :: ExpQ -> ExpQ -> StmtQ
-assertValidates schema value = noBindS $ ParensE <$>
+assertValidates schema value = noBindS $ parensE
   [| case validate $(varE $ mkName "graph") $schema $value of
        [] -> return ()
        es -> fail $ unlines es
@@ -389,11 +389,14 @@ generateObject decName name schema = case (propertiesList, schemaAdditionalPrope
     checkAdditionalProperties _ (Choice1of2 True) = [| return () |]
     checkAdditionalProperties _ (Choice1of2 False) = [| fail "additional properties are not allowed" |]
     checkAdditionalProperties value (Choice2of2 sch) = doE [assertValidates (lift sch) value]
+    -- TODO Once https://ghc.haskell.org/trac/ghc/ticket/10734 is
+    -- fixed, use a ‘let’ again for matchingPatterns and
+    -- isAdditionalProperty
     checkPatternAndAdditionalProperties patterns additional = noBindS
       [| let items = HM.toList $(varE obj) in forM_ items $ \(pname, value) -> do
-           let matchingPatterns = filter (flip PCRE.match (unpack pname) . patternCompiled . fst) $(lift patterns)
+           matchingPatterns <- return (filter (flip PCRE.match (unpack pname) . patternCompiled . fst) $(lift patterns))
            forM_ matchingPatterns $ \(_, sch) -> $(doE [assertValidates [| sch |] [| value |]])
-           let isAdditionalProperty = null matchingPatterns && pname `notElem` $(lift $ map fst $ HM.toList $ schemaProperties schema)
+           isAdditionalProperty <- return (null matchingPatterns && pname `notElem` $(lift $ map fst $ HM.toList $ schemaProperties schema))
            when isAdditionalProperty $(checkAdditionalProperties [| value |] additional)
       |]
     additionalPropertiesAllowed (Choice1of2 True) = True
