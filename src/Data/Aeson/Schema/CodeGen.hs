639779b07dc4182fe5c701e31c0b02401b8237e6
Christian Marie <christian@daisee.com>
2018-07-24 15:28:26 +1000

    Update for stack lts-11.4 with some TH CPP logic.

--- src/Data/Aeson/Schema/CodeGen.hs
+++ src/Data/Aeson/Schema/CodeGen.hs
@@ -17,7 +17,7 @@ import           Control.Arrow               (first, second)
 import           Control.Monad               (forM_, unless, when, zipWithM)
 import           Control.Monad.RWS.Lazy      (MonadReader (..),
                                               MonadWriter (..), evalRWST)
-import           Data.Aeson
+import           Data.Aeson hiding (Options)
 import           Data.Aeson.Types            (parse)
 import           Data.Char                   (isAlphaNum, isLetter, toLower,
                                               toUpper)
@@ -102,8 +102,10 @@ generateTopLevel graph = do
     let typeName = typeMap M.! name
     ((typeQ, fromJsonQ, toJsonQ), defNewtype) <- generateSchema (Just typeName) name schema
     when defNewtype $ do
-
-#if MIN_VERSION_template_haskell(2,11,0)
+#if MIN_VERSION_template_haskell(2,12,0)
+      let newtypeCon = normalC typeName [bangType (pure $ Bang NoSourceUnpackedness NoSourceStrictness) typeQ]
+      newtypeDec <- runQ $ newtypeD (cxt []) typeName [] Nothing newtypeCon [derivClause Nothing . fmap conT $ _derivingTypeclasses opts]
+#elif MIN_VERSION_template_haskell(2,11,0)
       let newtypeCon = normalC typeName [bangType (pure $ Bang NoSourceUnpackedness NoSourceStrictness) typeQ]
       newtypeDec <- runQ $ newtypeD (cxt []) typeName [] Nothing newtypeCon (mapM conT $ _derivingTypeclasses opts)
 #else
