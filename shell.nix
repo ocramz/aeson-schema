6190369090eb3e018885be0948e41e4471a4157d
Christian Marie <christian@daisee.com>
2018-10-30 09:34:12 +1100

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,46 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, aeson, attoparsec, base, bytestring
+      , containers, directory, fail, filepath, ghc-prim, hashable, hint
+      , HUnit, mtl, QuickCheck, regex-base, regex-compat, regex-pcre
+      , scientific, stdenv, syb, template-haskell, temporary
+      , test-framework, test-framework-hunit, test-framework-quickcheck2
+      , text, th-lift, transformers, unordered-containers, vector
+      }:
+      mkDerivation {
+        pname = "aeson-schema";
+        version = "0.4.1.2";
+        src = ./.;
+        enableSeparateDataOutput = true;
+        libraryHaskellDepends = [
+          aeson attoparsec base bytestring containers fail ghc-prim mtl
+          QuickCheck regex-base regex-compat regex-pcre scientific syb
+          template-haskell text th-lift transformers unordered-containers
+          vector
+        ];
+        testHaskellDepends = [
+          aeson attoparsec base bytestring containers directory filepath
+          hashable hint HUnit mtl QuickCheck regex-compat scientific
+          template-haskell temporary test-framework test-framework-hunit
+          test-framework-quickcheck2 text unordered-containers vector
+        ];
+        homepage = "https://github.com/Fuuzetsu/aeson-schema";
+        description = "Haskell JSON schema validator and parser generator";
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
